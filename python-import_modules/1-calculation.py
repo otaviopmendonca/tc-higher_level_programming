8300bfd2f925624ca9f7dd7d1b291b4d408ccf17
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-11-06 14:40:06 +0000

    feat: Adciona implementacao da calculadora

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 80
+b = 4
+add = add(a, b)
+sub = sub(a, b)
+mul = mul(a, b)
+div = div(a, b)
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add))
+    print("{} - {} = {}".format(a, b, sub))
+    print("{} * {} = {}".format(a, b, mul))
+    print("{} / {} = {}".format(a, b, div))
