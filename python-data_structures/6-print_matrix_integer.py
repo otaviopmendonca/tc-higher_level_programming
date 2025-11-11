6fcbc1e81d692ad21f4543b36b4b6243303d17f5
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-11-11 14:50:55 +0000

    feat: Adiciona funcao que imprime uma matriz de inteiros.

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        if len(row) == 0:
+            print()
+        else:
+            for i in range(len(row)):
+                if i == len(row) - 1:
+                    print("{:d}".format(row[i]))
+                else:
+                    print("{:d}".format(row[i]), end=" ")
