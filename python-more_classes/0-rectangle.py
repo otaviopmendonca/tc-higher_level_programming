d0785be9bdd71f8b8caba8fae92894c1724451e5
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-03 17:59:36 +0000

    feat: Adiciona uma classe vazia que representa um retangulo

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Rectangle class module
+This module defines an empty Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle.
+
+    This class is currently empty and serves as a placeholder.
+
+    Attributes:
+        None
+    """
+    pass
