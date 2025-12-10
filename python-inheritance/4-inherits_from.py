b95f1d23bd5fc4e9236b19faf5b7ba11577608a8
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-10 17:00:35 +0000

    feat: PEP8 validation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """
 4-inherits_from module.
-Contains the inherits_from function which checks for 
+Contains the inherits_from function which checks for
 inheritance (subclassing)excluding the base class itself.
 """
 
 
 def inherits_from(obj, a_class):
     """
-    Returns: True if obj is an instance of a subclass of a_class, 
+    Returns: True if obj is an instance of a subclass of a_class,
     False otherwise.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
