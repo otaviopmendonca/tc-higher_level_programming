57a181190ecbafd15a62bcdfc992661325dbfc46
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-10 13:49:43 +0000

    feat: PEP8 validation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 2-is_same_class module.
-Contains the is_same_class function which checks 
+Contains the is_same_class function which checks
 for exact object type equality.
 """
 
