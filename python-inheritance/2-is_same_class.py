f4448be19136ee445daecc84113162d02a91a879
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-10 13:40:00 +0000

    feat: Implementa is_same_class com verificacao de tipo (type)

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+2-is_same_class module.
+Contains the is_same_class function which checks for exact object type equality.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Checks if an object is exactly an instance of the specified class.
+
+    This function only returns True if the object's type is identical
+    to the specified class, excluding subclasses.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare the object's type against.
+
+    Returns:
+        True if the type of obj is exactly a_class, False otherwise.
+    """
+    return type(obj) is a_class
