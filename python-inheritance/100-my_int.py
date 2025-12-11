26dc1d4107d6be39d7fcefd785f3bbb33c7744d8
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-11 14:07:11 +0000

    feat: Implementa MyInt com operadores == e != invertidos

---
+++ python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+100-my_int module.
+Defines the class MyInt which inherits from int.
+"""
+
+
+class MyInt(int):
+    """
+    MyInt class inherits from int and inverts the behavior of == and !=.
+    """
+
+    def __eq__(self, value):
+        """
+        Overrides == operator. Returns the result of the standard != comparison.
+        """
+        return not super().__eq__(value)
+
+    def __ne__(self, value):
+        """
+        Overrides != operator. Returns the result of the standard == comparison.
+        """
+        return not super().__ne__(value)
