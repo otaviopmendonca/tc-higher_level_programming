4d0472c10276c41db122fca8ed285984ee94129b
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-12-11 17:53:12 +0000

    docs: Adiciona docstrings descritivos para classes e métodos

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,15 +1,41 @@
 #!/usr/bin/python3
+"""
+Task 0: Abstract Animal Class and its Subclasses
+Demonstrates the use of ABCs to create an abstract Animal class
+with an abstract method 'sound'
+"""
 from abc import ABC, abstractmethod
 
+
 class Animal(ABC):
+    """
+    Abstract class for all animals with the 'sound' method.
+    """
     @abstractmethod
     def sound(self):
+        """
+        Abstract method representing the sound the animal makes.
+        """
         pass
 
 class Dog(Animal):
+    """
+    Subclass of Animal.
+    Implements the abstract 'sound' method.
+    """
     def sound(self):
+        """
+        Returns the sound a Dog makes.
+        """
         return "Bark"
 
 class Cat(Animal):
+    """
+    Subclass of Animal.
+    Implements the abstract 'sound' method.
+    """
     def sound(self):
+        """
+        Returns the sound a Cat makes.
+        """
         return "Meow"
